042792d2bbfac0335dd5cd064c6a1f586360324c
Krzysztof Swiatly <swiatlyk@gmail.com>
2020-04-12 13:40:57 +0200

    Working warrior1

--- BattleFieldDotNetCore/Battle.cs
+++ BattleFieldDotNetCore/Battle.cs
@@ -4,33 +4,29 @@ namespace BattleFieldDotNetCore
 {
     public class Battle
     {
-         //correct access level
+        //correct access level
         public static void Start(Warrior warrior1, Warrior warrior2)
         {
             //implement random in fight
             while (warrior1.InitHealth > 0 && warrior2.InitHealth > 0)
             {
-                if (warrior2.InitHealth > 0)
+                if (warrior1.InitHealth > 0)
                 {
                     warrior1.Attack(warrior2);
-                    if (warrior1.InitHealth > 0)
+                    if (warrior2.InitHealth > 0)
                     {
                         warrior2.Attack(warrior1);
                     }
                     else
                     {
-
+                        Console.WriteLine($"{warrior1.WarriorName} wins!");
                     }
                 }
                 else
                 {
                     Console.WriteLine($"{warrior2.WarriorName} wins!");
-
                 }
-                Console.WriteLine($"{warrior1.WarriorName} wins!");
             }
-
-
         }
     }
 }
